e795ae3c9705b5677d80803d95b3f51304ef88a1
yjgaia <yjdev88@gmail.com>
2024-12-03 22:28:13 +0900

    Add ClanLogoInput component to ClanForm; implement logo upload and display functionality

--- app/forms/ClanForm.ts
+++ app/forms/ClanForm.ts
@@ -1,7 +1,11 @@
 import { DomNode } from "@common-module/app";
+import ClanLogoInput from "./ClanLogoInput.js";
 
 export default class ClanForm extends DomNode {
   constructor() {
     super(".clan-form");
+    this.append(
+      new ClanLogoInput({}),
+    );
   }
 }
